7bd92dde1518b1edf60d5f6c5bffba0ba52d4be1
LUAN FERREIRA DE SOUZA <bv3024644@ad.sbv.ifsp.edu.br>
2024-10-25 20:55:01 -0300

    Primeiro commit: projeto de placar

---
+++ generateData.js
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker';
+import fs from 'fs';
+
+
+const generateGameName = () => {
+  return `${faker.word.adjective()} ${faker.word.noun()}`;
+};
+const generatePlacares = (num) => {
+  const placares = [];
+  for (let i = 0; i < num; i++) {
+    placares.push({
+      id: i + 1,
+      usuario: faker.name.firstName(),
+      game:  generateGameName(),
+      score: faker.number.int({ min: 0, max: 5 }), // Corrigido
+    
+    });
+  }
+  return placares;
+};
+
+const placares = generatePlacares(10); // Gera 10 placares
+
+fs.writeFileSync('db.json', JSON.stringify({ placares }, null, 2));
+console.log('Dados gerados com sucesso!');
